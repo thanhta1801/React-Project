b7304154abb5be0bd5b09f21731f57a9b433f166
Thành Tạ <tanhuthanh1822@gmail.com>
2024-09-16 17:31:30 +0700

    updata11

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,13 +1,12 @@
 import ModalCreateUser from "./ModalCreateUser";
+import "./ManageUser.scss";
 
 const ManageUser = (props) => {
   return (
-    <div classNameName="manage-user-container">
-      <div classNameName="title">Manage Users</div>
-      <div classNameName="users-content">
-        <div>
-          <ModalCreateUser />
-        </div>
+    <div className="manage-user-container">
+      <div className="title">Manage Users</div>
+      <div className="users-content">
+        <ModalCreateUser />
         <div>Table Users</div>
       </div>
     </div>
--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -1,23 +1,39 @@
+/* eslint-disable jsx-a11y/alt-text */
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
+import { FcPlus } from "react-icons/fc";
 const ModalCreateUser = () => {
   const [show, setShow] = useState(false);
-  //   const [email, setEmail] = useState("");
-  //   const [passWord, setPassWord] = useState("");
-  //   const [userName, setUserName] = useState("");
+  /////////
+  const [email, setEmail] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [userName, setUserName] = useState("");
+  const [role, setRole] = useState("");
+  const [image, setImage] = useState("");
+  const [previewImage, setpreviewImage] = useState("");
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const handleUpLoadImage = (e) => {
+    if (e.target && e.target.files && e.target.files[0]) {
+      setpreviewImage(URL.createObjectURL(e.target.files[0]));
+    }
+  };
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
         Add New Users
       </Button>
 
-      <Modal show={show} onHide={handleClose} size="xl" backdrop="static">
+      <Modal
+        show={show}
+        onHide={handleClose}
+        size="xl"
+        backdrop="static"
+        className="modal-add-user"
+      >
         <Modal.Header closeButton>
           <Modal.Title>Add New User</Modal.Title>
         </Modal.Header>
@@ -25,28 +41,59 @@ const ModalCreateUser = () => {
           <form className="row g-3">
             <div className="col-md-6">
               <label className="form-label">Email</label>
-              <input type="email" className="form-control" />
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">Password</label>
-              <input type="password" className="form-control" />
+              <input
+                type="password"
+                className="form-control"
+                value={passWord}
+                onChange={(e) => setPassWord(e.target.value)}
+              />
             </div>
             <div className="col-md-6">
               <label className="form-label">UserName</label>
-              <input type="text" className="form-control" />
+              <input
+                type="text"
+                className="form-control"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
             </div>
             <div className="col-md-4">
               <label className="form-label">Role</label>
-              <select className="form-select">
-                <option selected value="USER">
-                  USER
-                </option>
+              <select
+                className="form-select"
+                onChange={(e) => setRole(e.target.value)}
+              >
+                <option value="USER">USER</option>
                 <option value="ADMIN">ADMIN</option>
               </select>
             </div>
             <div className="col-md-12">
-              <label className="form-label">Image</label>
-              <input type="file" />
+              <label className="form-label label-update-img" htmlFor="fileImg">
+                <FcPlus />
+                UpLoad File Image
+              </label>
+              <input
+                id="fileImg"
+                type="file"
+                hidden
+                onChange={(e) => handleUpLoadImage(e)}
+              />
+            </div>
+            <div className="col-md-12 img-preview">
+              {previewImage ? (
+                <img src={previewImage} />
+              ) : (
+                <span>Preview Image</span>
+              )}
             </div>
           </form>
         </Modal.Body>
